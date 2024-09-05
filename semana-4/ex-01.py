036170b963bd3c6bf36cc49df31ce52d61689ddd
Matheus Santos de Abreu <2400733@SALAS.AULAS>
2024-09-05 19:47:53 -0300

    completing second question

--- semana-4/ex-01.py
+++ semana-4/ex-01.py
@@ -29,4 +29,3 @@ conta.exibirSaldo()
 conta.depositarDinheiro(100)
 conta.exibirSaldo()
 conta.depositarDinheiro(-100)
-
---
+++ semana-4/ex-02.py
@@ -0,0 +1,26 @@
+# Exercício 2: Getters e Setters Simples
+
+# Crie uma classe Lampada com um atributo privado estado
+# (ligado/desligado). Implemente métodos getters e setters para o
+# atributo estado. O setter deve aceitar apenas os valores
+# "ligado" ou "desligado" e alterar o estado da lâmpada de
+# acordo.
+
+class Lampada:
+    def __init__(self):
+        self.__estado = 'desligado'
+
+    def set_Estado(self, estado):
+        if(estado == 'desligado' or estado == 'ligado'):
+            self.__estado = estado
+        else:
+            print('valor inválido')
+    
+    def get_Estado(self):
+        print(self.__estado)
+
+lampadaNova = Lampada()
+lampadaNova.get_Estado()
+lampadaNova.set_Estado('teste')
+lampadaNova.set_Estado('ligado')
+lampadaNova.get_Estado()
